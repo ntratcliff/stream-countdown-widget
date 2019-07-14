3e1a848d99291cd3fd14b021b238e6616b38c2e5
Noah Ratcliff <me@nratcliff.com>
2019-07-14 11:27:44 -0400

    set time in url

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="root">
 		<Countdown
-			:hours="0"
-			:minutes="15"
-			:seconds="0"
+			:hours="hours"
+			:minutes="minutes"
+			:seconds="seconds"
 			:display-hours="false"
 			minutes-text=":"
-			seconds-text=""
+			seconds-text
 		/>
 		<span class="minutes-text">minutes</span>
 	</div>
@@ -18,6 +18,29 @@ import Countdown from './components/Countdown.vue'
 export default {
 	components: {
 		Countdown
+	},
+	data () {
+		return {
+			hours: 0,
+			minutes: 0,
+			seconds: 0
+		}
+	},
+	created () {
+		// get hms from query string
+		let params = new URLSearchParams(window.location.search.substring(1))
+
+		if (params.get('h') != null) {
+			this.hours = parseInt(params.get('h'))
+		}
+
+		if (params.get('m') != null) {
+			this.minutes = parseInt(params.get('m'))
+		}
+
+		if (params.get('s') != null) {
+			this.seconds = parseInt(params.get('s'))
+		}
 	}
 }
 </script>
